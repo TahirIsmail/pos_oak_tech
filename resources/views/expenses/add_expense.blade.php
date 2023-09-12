a02b4c6c2b11f1170d0244b7e14f495a4a987d0f
TahirIsmail <vjtary104@gmail.com>
2023-09-12 16:40:38 +0500

    manifest push

--- resources/views/expenses/add_expense.blade.php
+++ resources/views/expenses/add_expense.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.layout')
 @section('content')
-    <addexpensecomponent :expense_categories = "{{json_encode($expenseCategories)}}" :expense_data= "{{json_encode($expenses_data)}}" :selected_expense_id="{{json_encode($selectedExpenseId)}}"></addexpensecomponent>
+    <addexpensecomponent :expense_categories = "{{json_encode($expenseCategories)}}" :expense_data= "{{json_encode($expenses_data)}}" :selected_expense_id="$selectedExpenseId"></addexpensecomponent>
 @endsection
