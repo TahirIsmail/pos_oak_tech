ad0871125b820dba4b3db59ca1abe74504427740
TahirIsmail <vjtary104@gmail.com>
2023-09-12 18:15:15 +0500

    vue component

--- resources/assets/js/components/expense/add_expense_component.vue
+++ resources/assets/js/components/expense/add_expense_component.vue
@@ -1,5 +1,5 @@
 <template>
-    <div >
+    <div>
         <form @submit.prevent="submit_form">
             <div class="d-flex flex-wrap mb-4">
                 <div class="mr-auto">
@@ -22,7 +22,7 @@
                 </div>
             </div>
             <p v-html="server_errors" v-bind:class="[error_class]"></p>
-            <div class="form-row mb-2" >
+            <div class="form-row mb-2">
                 <div class="form-group col-sm-12 col-md-4">
                     <label for="name">{{ $t("Expense Name") }}</label>
                     <input type="text" name="expense_name" v-model="expense_name" v-validate="'max:65535'"
@@ -41,11 +41,11 @@
                 </div>
                 <div class="form-group col-sm-12 col-md-4">
                     <label for="expense_category">{{ $t("Choose Expense Category") }}</label>
-                    <select v-model="expense_category" v-validate="'required'"
-                        class="form-control form-control-custom" placeholder="Choose Expense Category..">
+                    <select v-model="expense_category" v-validate="'required'" class="form-control form-control-custom"
+                        placeholder="Choose Expense Category..">
                         <option value="" disabled>Choose Expense Category..</option>
-                        <option v-for="(expense_item, index) in expense_categories" v-bind:value="expense_item.id" placeholder="Choose Expense Category.."
-                            v-bind:key="index">
+                        <option v-for="(expense_item, index) in expense_categories" v-bind:value="expense_item.id"
+                            placeholder="Choose Expense Category.." v-bind:key="index">
                             {{ expense_item.id }} - {{ expense_item.name }}
                         </option>
                     </select>
@@ -53,18 +53,61 @@
                     }}</span>
                 </div>
             </div>
-            
-               
+            <div class="form-row mb-2">
+                <div class="form-group col-md-3">
+                    <label for="name">{{ $t("Notes") }}</label>
+                    <textarea name="notes" v-model="notes" v-validate="'required|max:65535'"
+                        class="form-control form-control-custom" rows="5"
+                        :placeholder="$t('Notes on this Current Expense')"></textarea>
 
-            
+                    <span v-bind:class="{ error: errors.has('notes') }">{{
+                        errors.first("notes")
+                    }}</span>
+                   
+                </div>
+            </div>
+            <div class="form-row mb-2">
+                <div class="form-group col-md-3">
+                    <label for="expense_date">{{ $t("Expense Date") }}</label>
+                    <date-picker  v-model="expense_date"
+                        name="expense_date"
+                        v-validate="'required|date_format:yyyy-MM-dd'"
+                        input-class="form-control form-control-custom bg-white"
+                        :placeholder="$t('Please enter Expense Date')" autocomplete="off"></date-picker>
+                    <span v-bind:class="{ 'error': errors.has('Expense Date') }">{{ errors.first('Expense Date')
+                    }}</span>
+                </div>
+                
+            </div>
+
+
+
 
 
         </form>
+        <modalcomponent v-if="show_modal" v-on:close="show_modal = false">
+            <template v-slot:modal-header>
+                {{ $t("Confirm") }}
+            </template>
+            
+            <template v-slot:modal-footer>
+                <button type="button" class="btn btn-light" @click="$emit('close')">
+                    Cancel
+                </button>
+                <button type="button" class="btn btn-primary" @click="$emit('submit')" v-bind:disabled="processing == true">
+                    <i class="fa fa-circle-notch fa-spin" v-if="processing == true"></i>
+                    Continue
+                </button>
+            </template>
+        </modalcomponent>
 
     </div>
 </template>
 <script>
 "use strict";
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+import moment from "moment";
 export default {
     data() {
         return {
@@ -78,15 +121,17 @@ export default {
 
             api_link: "",
             expense_slack: null,
-            expense_name:"",
-            expense_category:"",
-            expense_amount:""
+            expense_name: "",
+            expense_category: "",
+            expense_amount: "",
+            expense_date:"",
+            notes:"",
         }
     },
-    props:{
-        expense_categories : Array,
+    props: {
+        expense_categories: Array,
         expense_data: Array,
-        selected_expense_id:Array,
+        selected_expense_id: Array,
     }
 }
 </script>
